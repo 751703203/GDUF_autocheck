79f6d77f2f3b6aec650c3ba755262b03301eb612
神仙哥哥 <83892346+751703203@users.noreply.github.com>
2021-11-30 15:37:56 +0800

    Update index.py
    针对我自己

--- index.py
+++ index.py
@@ -1,8 +1,5 @@
 import requests
-import json
 import time
-import ast
-
 
 # 易班打卡
 def main(checkinfo):
@@ -11,83 +8,50 @@ def main(checkinfo):
     i = 0
     L = list(checkinfo.keys())
     L.sort()
-    err = {}
     while i < len(checkinfo):
-        post(checkinfo["{0}".format(L[i])][0],
-             checkinfo["{0}".format(L[i])][1])
-    # 先判断再推送
-        if yb_result["code"] == 555555:
-            err['{0}'.format(L[i])] = yb_result["msg"]
-        elif yb_result["code"] != 0:
-            err['{0}'.format(L[i])] = yb_result["msg"]
+        post(checkinfo["%s" % L[i]][0], checkinfo["%s" % L[i]][1])
         i = i+1
-    if len(err) != 0:
-        print("{0}打卡失败".format(date))
-        tuisong(err)
-    else:
-        print("{0}打卡完成".format(date))
 
 
-def post(loginToken, studentID):
+def post(loginToken, address):
     global yb_result
     session = requests.Session()
-    url_1 = "http://f.yiban.cn/iapp378946/i/{0}".format(loginToken)
-    a = session.get(url=url_1, allow_redirects=False)
-    url_2 = "http://f.yiban.cn{0}".format(a.headers['Location'])
-    b = session.get(url=url_2, allow_redirects=False)
-    if "Location" not in b.headers:
-        yb_result = {'code': 555555, 'msg': 'loginToken错误，请修改'}
+    url_1 = "http://f.yiban.cn/iapp378946/i/%s" % loginToken
+    a = session.get(url=url_1, headers=UA, allow_redirects=False)
+    url_2 = a.headers['Location']
+    header_loginToken = {"loginToken": "%s" % loginToken}
+    b = session.get(url=url_2, headers=header_loginToken,
+                    allow_redirects=False)
+    url_get = b.headers["Location"]
+    c = session.get(url=url_get, headers=UA, allow_redirects=False)
+    studentID = c.headers['Location'].split('studentID=')[1]
+    url_bind = "https://ygj.gduf.edu.cn/Handler/device.ashx?flag=checkBindDevice"
+    bind = session.get(url=url_bind, headers=UA)
+    url_save = "https://ygj.gduf.edu.cn/Handler/health.ashx?"
+    data_yb_save = {
+        "flag": "save",
+        "studentID": "%s" % studentID,
+        "date": "%s" % date,
+        "health": "体温37.3℃以下（正常）",
+        "address": "广东省广州市天河区迎福路78号靠近龙圣学校",
+        "isTouch": "否",
+        "isPatient": "不是"
+    }
+    if not address:
+        yb_result = session.post(
+            url=url_save, headers=UA, data=data_yb_save).json()
         return yb_result
     else:
-        url_get = b.headers["Location"]
-        c = session.get(url=url_get, allow_redirects=False)
-        url_bind = "https://ygj.gduf.edu.cn/Handler/device.ashx?flag=checkBindDevice"
-        bind = session.get(url=url_bind)
-        url_save = "https://ygj.gduf.edu.cn/Handler/health.ashx?"
-        data_yb_save = {
-            "flag": "save",
-            "studentID": "{0}".format(studentID),
-            "date": "{0}".format(date),
-            "health": "体温37.3℃以下（正常）",
-            "address": "广东省广州市天河区迎福路305号靠近龙圣学校",
-            "isTouch": "否",
-            "isPatient": "不是"
-        }
-        d = session.post(url=url_save, data=data_yb_save)
-        yb_result = json.loads(
-            str(d.content, encoding="utf-8").encode("utf-8").decode('unicode_escape'))
+        data_yb_save["address"] = address
+        yb_result = session.post(
+            url=url_save, headers=UA, data=data_yb_save).json()
         return yb_result
-
-
-# 推送判断
-def tuisong(err):
-    # Server酱推送
-    api = "https://sc.ftqq.com/"
-    data = {"text": "易班打卡异常提醒", "desp": str(err)}
-    # req = requests.post(api, data=data)
-    print(data)
-
-
-# 未完善的改良
-def get_sectets(users):
-    while True:
-        try:
-            info = users.split(',')
-            checkinfo[info[0]] = [info[1], info[2]]
-            return checkinfo
-        except:
-            break
 
 
 if __name__ == "__main__":
-    # sectets字段录入
-    users = input()
-    users = ast.literal_eval(users)
+    USERS = {'张洵': ['cf8224510e7450226cb39601a4679079', '广东省广州市天河区华夏路92号靠近高德置地广场·夏']}
+    UA = {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 yiban_iOS/4.9.10"
+    }
 
-    # 调用改良
-    # checkinfo = {}
-    # get_sectets(users)
-    # print(checkinfo)
-
-    # 调用原版
-    main(users)
+    main(USERS)
